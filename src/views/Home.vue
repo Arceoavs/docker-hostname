80cd5a958ba2527f06a708166061ca9b610486ab
Arnold Arz <arnoarz99@gmail.com>
2021-03-05 20:32:19 +0100

    🏗

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,12 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+.home
+  img(alt='Vue logo' src='../assets/logo.png')
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
 export default {
   name: 'Home',
-  components: { HelloWorld },
+  components: {},
   data() {
     return {};
   }
